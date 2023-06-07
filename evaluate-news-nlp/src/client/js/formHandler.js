5a5041b7da267f10c4972e0ece3b8f1513a0ca4a
hunde02 <matthias.hundeloh@bertelsmann.de>
2023-06-07 17:06:56 +0200

    feat: api fetch handling and styling

--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -3,6 +3,7 @@ const postData = async (url = '', data = {}) => {
     const response = await fetch(url, {
         method: 'POST',
         credentials: 'same-origin',
+        cache: 'no-cache',
         headers: {
             'Content-Type': 'application/json',
         },
@@ -14,20 +15,53 @@ const postData = async (url = '', data = {}) => {
         console.log('Error', error);
     }
 }
+
+// prepare table row element
+const prepareTableRow = (rowTag, cellTag, data) => {
+    let row = document.createElement(rowTag);
+    for (const element of data) {
+        let cell = document.createElement(cellTag);
+        cell.innerHTML = element;
+        row.appendChild(cell);
+    }
+    return row;
+
+}
+
+// prepare data for UI
+const prepareData = (data) => {
+
+    let table = document.createElement('table');
+    if (data.sentence_list !== undefined) {
+        let table_head = document.createElement('thead');
+        let head_row = prepareTableRow('tr', 'th', ['Level', 'Text', 'Score tag', 'Agreement', 'Confidence']);
+        table_head.appendChild(head_row);
+        table.appendChild(table_head);
+        let table_body = document.createElement('tbody');
+        let first_row = prepareTableRow('tr', 'td', ['Global', data.irony, data.score_tag, data.agreement, data.confidence])
+        table_body.appendChild(first_row);
+        for (const sentence of data.sentence_list) {
+            let table_row = prepareTableRow('tr', 'td', ['Sentence', sentence.text, sentence.score_tag, sentence.agreement, sentence.confidence])
+            table_body.appendChild(table_row);
+            for (const segment of sentence.segment_list) {
+                let table_row = prepareTableRow('tr', 'td', ['Segment', segment.text, segment.score_tag, segment.agreement, segment.confidence])
+                table_body.appendChild(table_row)
+            }
+        }
+        table.appendChild(table_body)
+    }
+    return table;
+}
+
 /* Update UI  */
 const updateUI = async (formText) => {
     const results = document.querySelector('#results');
-
-    const request = await postData('http://localhost:8080/analyze', {formText})
-        .then(res => res.json())
-        .then(function(res) {
-            console.log('Response from analyze', JSON.stringify(res))
-            return res.message;
-        });
+    results.innerHTML = '';
     try {
-        const allData = await request;
-        if (allData !== undefined) {
-            results.innerHTML = JSON.stringify(allData);
+        const request = await postData('http://localhost:8080/analyze', {formText});
+        const data = await request.json();
+        if (data !== undefined) {
+            results.appendChild(prepareData(data));
         } else {
             results.innerHTML = 'Nothing here!';
         }
@@ -36,21 +70,12 @@ const updateUI = async (formText) => {
     }
 }
 
-
 function handleSubmit(event) {
     event.preventDefault()
 
-    console.log("::: Form Submitted :::")
-
     // check what text was put into the form field
     let formText = document.getElementById('input_text').value
-    console.log(formText)
     updateUI(formText)
-    // postData('http://localhost:8080/analyze', {formText})
-    //     .then(res => res.json())
-    //     .then(function(res) {
-    //         document.getElementById('results').innerHTML = JSON.stringify(res.message)
-    //     })
 }
 
 export { handleSubmit }
--- evaluate-news-nlp/src/server/index.js
+++ evaluate-news-nlp/src/server/index.js
@@ -25,46 +25,46 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
-function analyzeText(req, resp) {
-    const text = req.body;
-    const formdata = new FormData();
-    formdata.append("key", apikey);
-    formdata.append("txt", text.formText);
-    formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+/* asynchronous function for calling text analysis api */
+async function analyzeText(req, resp) {
+    try {
+        const text = req.body;
+        const formdata = new FormData();
+        formdata.append("key", apikey);
+        formdata.append("txt", text.formText);
+        formdata.append("lang", "en");  // 2-letter code, like en es fr ...
 
-    const requestOptions = {
-        method: 'POST',
-        body: formdata,
-        redirect: 'follow'
-    };
+        const requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
 
-    const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-        .then(response => ({
-            status: response.status,
-            body: response.json()
-        }))
-        .then(({ status, body }) => {
-            console.log('success', status, body)
-            resp.send({message: body});
-        })
-        .catch(error => {
-            console.log('error', error)
-            resp.send({message: 'Error occurred whilst request'})
-        });
+        const response = await fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions);
+        const result = await response.json();
+
+        resp.send(result);
+    } catch (error) {
+        console.error("Error:", error);
+        resp.send(error);
+    }
 }
-
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('dist/index.html'))
-})
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
+/* GET request route for homepage */
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('dist/index.html'))
+})
+
+/* POST request route for analysis */
 app.post('/analyze', analyzeText);
 
+/* GET request route for test */
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
